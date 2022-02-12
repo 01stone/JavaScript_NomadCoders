2dbda409d498428c49ca02ddd7f46fff0a6c71a4
1stone <qkatorxhrl@gmail.com>
2022-02-12 23:23:42 +0900

    feat : day1

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,8 @@
-alert("hello JavaScript");
+const amIStudied = true;
+console.log(amIStudied);
+
+const amIFat = null;  // defined
+console.log(amIFat);
+
+let something;        // undefined
+console.log(something);
